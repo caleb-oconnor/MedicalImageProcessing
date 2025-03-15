0c33025285706130f34bf6544fa2f63e9b8e0d52
caleb-oconnor <caleb.oconnor2@gmail.com>
2025-03-15 13:05:33 -0500

    Changed - combined 3 mesh based registrations in to one

--- MedicalImageProcessing/Registration/rigidregistration.py
+++ MedicalImageProcessing/Registration/rigidregistration.py
@@ -20,143 +20,73 @@ import SimpleITK as sitk
 
 from open3d.geometry import PointCloud
 from open3d.utility import Vector3dVector
-from open3d.pipelines.registration import registration_icp, TransformationEstimationPointToPoint, ICPConvergenceCriteria
+from open3d.pipelines.registration import (registration_icp, ICPConvergenceCriteria,
+                                           TransformationEstimationPointToPlane, TransformationEstimationPointToPoint)
 
 
-class ICPvtk(object):
-    def __init__(self, ref, mov):
-        self.source = mov
-        self.target = ref
+class ICP(object):
+    def __init__(self, source, target, matrix=None):
+        self.source = source
+        self.target = target
 
-        self.landmarks = int(np.round(len(mov.points)/10))
-        self.distance = 1e-5
-        self.iterations = 1000
+        self.matrix = matrix
 
-        self.icp = vtk.vtkIterativeClosestPointTransform()
+        self.icp = None
 
-        self.reverse_transform = False
+    def compute_com(self):
+        translation = np.asarray(self.mov.center - self.ref.center)
 
-    def update_parameters(self, landmarks=None, distance=None, iterations=None):
-        if landmarks:
-            self.landmarks = landmarks
+        self.matrix = np.identity(4)
+        self.matrix[:3, 3] = translation
 
-        if distance:
-            self.distance = distance
+    def compute_vtk(self, distance=10, iterations=1000, landmarks=None, com_matching=True):
+        if landmarks is None:
+            landmarks = int(np.round(len(self.target.points) / 10))
 
-        if iterations:
-            self.iterations = iterations
-
-    def compute_icp(self, com_matching=True):
         self.icp.SetSource(self.source)
         self.icp.SetTarget(self.target)
         self.icp.GetLandmarkTransform().SetModeToRigidBody()
         self.icp.SetCheckMeanDistance(1)
         self.icp.SetMeanDistanceModeToRMS()
-        self.icp.SetMaximumNumberOfLandmarks(self.landmarks)
-        self.icp.SetMaximumMeanDistance(self.distance)
-        self.icp.SetMaximumNumberOfIterations(self.iterations)
+        self.icp.SetMaximumNumberOfLandmarks(landmarks)
+        self.icp.SetMaximumMeanDistance(distance)
+        self.icp.SetMaximumNumberOfIterations(iterations)
         self.icp.SetStartByMatchingCentroids(com_matching)
         self.icp.Modified()
         self.icp.Update()
 
-        matrix = pv.array_from_vtkmatrix(self.icp.GetMatrix())
-        if self.reverse_transform:
-            matrix = np.linalg.inv(matrix)
+        self.matrix = pv.array_from_vtkmatrix(self.icp.GetMatrix())
 
-        return matrix
+    def compute_o3d(self, distance=10, iterations=1000, rmse=1e-7, fitness=1e-7, method='point', com_matching=True):
+        ref_pcd = PointCloud()
+        ref_pcd.points = Vector3dVector(self.source.points)
 
-    def compute_error(self):
-        matrix = pv.array_from_vtkmatrix(self.icp.GetMatrix())
-        new_source = self.source.transform(matrix, inplace=False)
+        mov_pcd = PointCloud()
+        mov_pcd.points = Vector3dVector(self.target.points)
+        mov_pcd.normals = Vector3dVector(np.asarray(self.target.point_normals))
 
-        closest_cells, closest_points = self.target.find_closest_cell(new_source.points, return_closest_point=True)
-        d_exact = np.linalg.norm(new_source.points - closest_points, axis=1)
-        new_source["distances"] = d_exact
-        return {'Min': np.min(d_exact), 'Mean': np.mean(d_exact), 'Max': np.max(d_exact)}
-
-
-class ICPo3d(object):
-    def __init__(self, ref_pv=None, mov_pv=None):
-        self.ref_pcd = PointCloud()
-        self.ref_pcd.points = Vector3dVector(ref_pv.points)
-
-        self.mov_pcd = PointCloud()
-        self.mov_pcd.points = Vector3dVector(mov_pv.points)
-        self.mov_pcd.normals = o3d.utility.Vector3dVector(np.asarray(mov_pv.point_normals))
-
-        self.distance = 1
-        self.iterations = 1000
-        self.rmse = 1e-7
-        self.fitness = 1e-7
-
-        self.initial_transform = None
-        self.compute_com()
-
-        self.registration = None
-        self.parameter_results = {'transform': None, 'fitness': None, 'rmse': None}
-
-    def set_initial_transform(self, transform):
-        self.initial_transform = transform
-
-    def set_icp_settings(self, distance=None, iterations=None, rmse=None, fitness=None):
-        if max_distancee:
-            self.distance = distance
-
-        if max_iteration:
-            self.iterations = iterations
-
-        if rmse:
-            self.rmse = rmse
-
-        if fitness:
-            self.fitness = fitness
-
-    def compute(self, com_matching=True, method='point'):
+        initial_transform = np.identity(4)
         if com_matching:
-            c = self.mov_pcd.get_center() - self.ref_pcd.get_center()
-            self.initial_transform = np.asarray([[1, 0, 0, c[0]], [0, 1, 0, c[1]], [0, 0, 1, c[2]], [0, 0, 0, 1]])
-        else:
-            self.initial_transform = np.identity(4, dtye=np.float32)
+            translation = mov_pcd.get_center() - ref_pcd.get_center()
+            initial_transform[:3, 3] = translation
 
         if method == 'point':
-            self.registration = registration_icp(self.ref_pcd, self.mov_pcd, self.distance, self.initial_transform,
-                                                 TransformationEstimationPointToPoint(),
-                                                 ICPConvergenceCriteria(max_iteration=self.iterations,
-                                                                        relative_rmse=self.rmse,
-                                                                        relative_fitness=self.fitness))
+            self.icp = registration_icp(ref_pcd, mov_pcd, distance, initial_transform,
+                                        TransformationEstimationPointToPoint(),
+                                        ICPConvergenceCriteria(max_iteration=iterations,
+                                                               relative_rmse=rmse,
+                                                               relative_fitness=fitness))
         else:
-            self.registration = registration_icp(self.ref_pcd, self.mov_pcd, self.distance, self.initial_transform,
-                                                 TransformationEstimationPointToPlane())
+            self.icp = registration_icp(ref_pcd, mov_pcd, distance, initial_transform,
+                                        TransformationEstimationPointToPlane())
+        self.matrix = self.icp.transformation
 
-        self.parameter_results['transform'] = self.registration.transformation
-        self.parameter_results['fitness'] = self.registration.fitness
-        self.parameter_results['rmse'] = self.registration.inlier_rmse
-
-        matrix = self.parameter_results['transform']
-
-        return matrix
-
-    def correspondence_array(self):
-        return self.registration.correspondence_set
-
-
-class MeshCenterOfMass(object):
-    def __init__(self, ref, mov):
-        self.ref = ref
-        self.mov = mov
-
-        self.matrix = None
-
-    def com_transfer(self):
-        ref_com = self.ref.center
-        mov_com = self.mov.center
-
-        translation = mov_com - ref_com
-
-        self.matrix = np.identity(4)
-        self.matrix[0, 3] = -translation[0]
-        self.matrix[1, 3] = -translation[1]
-        self.matrix[2, 3] = -translation[2]
-
+    def get_matrix(self):
         return self.matrix
 
+    def get_correspondence_set(self):
+        if hasattr(self.icp, 'correspondence_set'):
+            return self.icp.correspondence_set
+
+        else:
+            return None
